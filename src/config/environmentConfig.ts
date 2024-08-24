3222c9e762fc73f0eac92a9edd89491d4817e731
abhi02-90 <asmaster093@gmail.com>
2024-08-23 23:38:39 -0500

    fix: Test env for units tests

--- src/config/environmentConfig.ts
+++ src/config/environmentConfig.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const envFound = dotenv.config();
-if (envFound.error) {
+if (envFound.error && process.env.NODE_ENV != "test") {
   throw new Error("Couldn't find .env file");
 }
 
